a10af210f4ecc29a7b3c99dbf2561c80fb7dcea2
Boni-amin <boniamin.info@gmail.com>
2023-08-02 16:41:50 +0600

    some code added

---
+++ example.js
@@ -0,0 +1,25 @@
+
+/*
+        ---- code run tarminal ---
+1. node filename
+2. npm init --y 
+3. npm install nodemon -D
+
+json file = "start-dev": "nodemon example.js"
+
+npm run start-dev
+
+
+new file .gitignore
+
+git remote -v for link
+
+
+
+
+*/
+
+
+console.log("Hello World")
+console.log("Hello World ,,, ")  
+console.log("Hello World ,,, ")  
